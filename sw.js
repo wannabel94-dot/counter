49ca64c6a324cc93fe3026063a816c5551d2c7c8
wannabel94-dot <w.annabel94@gmail.com>
2026-02-01 10:52:46 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("nfc-counter").then(cache =>
+      cache.addAll(["./", "./index.html"])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
